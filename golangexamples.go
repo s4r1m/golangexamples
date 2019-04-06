604a0b469f663acb244869a4920d687dded6e4d9
s4r1m <sarimsohail81@gmail.com>
2019-04-06 22:24:58 +0500

    Completed assignment

--- golangexamples.go
+++ golangexamples.go
@@ -1,9 +1,27 @@
 package golangexamples
 
-func ConcatSlice(str string) string {
-	return str
+import (
+	"fmt"
+	"strings"
+)
+
+func ConcatSlice(slicetoConcat []byte) string {
+	var str []string
+	for i := 0; i < len(slicetoConcat); i++ {
+		str = append(str, string(slicetoConcat[i]))
+
+	}
+	return strings.Join(str, "-")
 }
 
-func Encrypt() {
-
+func Encrypt(slicetoEncrypt []byte, ceaserCount int) {
+	var str []string
+	for i := 0; i < len(slicetoEncrypt); i++ {
+		s := int(slicetoEncrypt[i]) + ceaserCount
+		if s > 'z' {
+			s = (s - 26)
+		}
+		str = append(str, string(s))
+	}
+	fmt.Println(str, "\n")
 }
